7216e8f7dd21c72a3116ac91a702b57479f9954b
Abhang Paturkar <abhangpaturkar0709@gmail.com>
2024-04-11 23:13:57 +0530

    Updated

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -1,26 +1,29 @@
 class Solution {
     public static String longestCommonPrefix(String[] strs) {
-        String prefix = "";
-        if (strs.length > 0) {
-            for (int i = 0; i < strs[0].length(); i++) {
-                char ch = strs[0].charAt(i);
-                for (int j = 1; j < strs.length; j++) {
-                    // Check if character equals or not || if length of string if greater than 'i'
-                    if (ch != strs[j].charAt(i) || (i > strs[j].length())) {
-                        return prefix;
-                    }
-                    // if(!(strs[j].length() <= i+1) && !(ch == strs[j].charAt(i))){
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
 
-                    // }
-                }
-                prefix += ch;
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            int j = 0;
+            while (j < prefix.length() && j < strs[i].length() && prefix.charAt(j) == strs[i].charAt(j)) {
+                j++;
+            }
+
+            prefix = prefix.substring(0, j);
+
+            if (prefix.isEmpty()) {
+                break;
             }
         }
+
         return prefix;
     }
 
     public static void main(String[] args) {
-        String strs[] = { "flower", "flowar", "flight" };
+        String strs[] = { "a" };
         System.out.println(longestCommonPrefix(strs));
     }
 }
