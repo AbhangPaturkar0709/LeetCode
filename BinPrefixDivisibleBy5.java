cbdb526d3a7712c90f0b024b7740b416031bd6a0
Abhang Paturkar <155937375+AbhangPaturkar2023BIT508@users.noreply.github.com>
2024-09-05 22:21:58 +0530

    .\BinPrefixDivisibleBy5.java

---
+++ BinPrefixDivisibleBy5.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        int n = nums.length;
+        List<Boolean> ans = new ArrayList<>();
+        int num = 0;
+
+        for(int i = 0; i < n; i++){
+            num = (num * 2 + nums[i]) % 5;
+            ans.add(num == 0);
+        }
+        return ans;
+        
+    }
+}
